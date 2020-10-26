924e0a80483a899563449564e4710d6ae0491b04
SOUHTYYY <leshka.l1337@gmail.com>
2020-10-26 17:47:50 +0300

    Add validation for creating Task with Notification

--- src/screens/Workspace/components/CreateTask/index.tsx
+++ src/screens/Workspace/components/CreateTask/index.tsx
@@ -8,6 +8,7 @@ import { ColumnType } from '../../types';
 
 // Compnents
 import AddIcon from '@material-ui/icons/Add';
+import { Notification } from '../../../../helpers/Notification';
 
 interface IProps {
   columns: ColumnType[];
@@ -19,18 +20,26 @@ export const CreateTask: React.FC<IProps> = (props: IProps) => {
   const [name, setName] = React.useState<string>('');
   const [visible, setVisible] = React.useState(false);
 
+  const [open, setOpen] = React.useState<boolean>(false);
+  const [notificationText, setNotificationText] = React.useState<string>('');
+
   const handleSetName = (e: any) => setName(e.target.value);
 
   const handleSetVisible = () => setVisible(true);
   const handleSetUnVisible = () => setVisible(false);
 
   const handleCreateTask = () => {
+    if (!name.length) {
+      setOpen(true);
+      setNotificationText('Please enter the task name!');
+      return;
+    }
     const idx = props.columns.findIndex((el) => el.id === props.columnID);
     let newArray = [...props.columns];
 
     const items = newArray[idx].items;
 
-    newArray[idx].items = [...items, { id: new Date().toString(), content: name }];
+    newArray[idx].items = [...items, { id: new Date().toString(), content: name, markers: [] }];
 
     props.setColumns(newArray);
 
@@ -39,19 +48,22 @@ export const CreateTask: React.FC<IProps> = (props: IProps) => {
   };
 
   return (
-    <Container>
-      {visible && (
-        <TextArea
-          autoFocus
-          onChange={handleSetName}
-          value={name}
-          placeholder="Enter name for this task"
-        />
-      )}
-      <Button onClick={visible ? handleCreateTask : handleSetVisible}>
-        <AddIcon />
-        {visible ? 'Add Task' : 'Create new Task'}
-      </Button>
-    </Container>
+    <>
+      <Container>
+        {visible && (
+          <TextArea
+            autoFocus
+            onChange={handleSetName}
+            value={name}
+            placeholder="Enter name for this task"
+          />
+        )}
+        <Button visible={visible} onClick={visible ? handleCreateTask : handleSetVisible}>
+          <AddIcon />
+          {visible ? 'Add Task' : 'Create new Task'}
+        </Button>
+      </Container>
+      <Notification isOpen={open} setClose={setOpen} text={notificationText} />
+    </>
   );
 };
